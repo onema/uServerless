f66e5ebffdb4c4a3e430584818315bd5cf854729
Juan Manuel Torres <kinojman@gmail.com>
2017-08-31 21:24:34 -0700

    Updates to the ApiGateway handler

--- src/main/scala/onema/serverlessbase/core/function/ApiGatewayHandler.scala
+++ src/main/scala/onema/serverlessbase/core/function/ApiGatewayHandler.scala
@@ -4,38 +4,42 @@ import java.util
 
 import com.amazonaws.serverless.proxy.internal.model.{AwsProxyRequest, AwsProxyResponse}
 import com.amazonaws.services.lambda.runtime.Context
-import onema.serverlessbase.core.json.Implicits.AnyClassToJsonString
+import com.typesafe.scalalogging.Logger
+import org.apache.http.HttpStatus
 import onema.serverlessbase.exception.HandleRequestException
 import onema.serverlessbase.model.ErrorMessage
-import org.apache.http.HttpStatus
-import com.typesafe.scalalogging.Logger
+import onema.serverlessbase.core.json.Implicits._
 
 import scala.util.{Failure, Success, Try}
 
 trait ApiGatewayHandler {
 
-  // --- Fields ---
-  val log = Logger("serverless")
+  //--- Fields ---
+  val log = Logger("apigateway-handler")
 
-  def handle(request: AwsProxyRequest): AwsProxyResponse
+  //--- Methods ---
+  def handleRequest(request: AwsProxyRequest): AwsProxyResponse
 
-  def handleRequest(request: AwsProxyRequest, context: Context): AwsProxyResponse = {
-    Try(handle(request)) match {
-      case Success(response) =>
-        log.info(s"Success Case")
-        response
-      case Failure(e) => e match {
-        case ex: HandleRequestException =>
-          log.info("Handle Error")
-          ex.httpResponse
-        case _ =>
-          log.info("Internal Server Error")
-          new AwsProxyResponse(
-            HttpStatus.SC_INTERNAL_SERVER_ERROR,
-            new util.HashMap[String, String](),
-            ErrorMessage("Internal server errors, check the logs for more information.").toJson
-          )
-      }
+  def handle(request: AwsProxyRequest, context: Context): AwsProxyResponse = {
+    Try(handleRequest(request)) match {
+      case Success(response) => response
+      case Failure(e: Throwable) => handleFailure(e)
+    }
+  }
+
+  def handleFailure(exception: Throwable): AwsProxyResponse = {
+    log.error(exception.getStackTrace.mkString)
+    log.error(exception.getMessage)
+    exception match {
+      case ex: HandleRequestException =>
+        ex.httpResponse
+      case _ =>
+        log.info("Internal Server Error")
+        new AwsProxyResponse(
+          HttpStatus.SC_INTERNAL_SERVER_ERROR,
+          new util.HashMap[String, String](),
+          ErrorMessage("Internal server errors, check the logs for more information.").toJson
+        )
     }
   }
 }
