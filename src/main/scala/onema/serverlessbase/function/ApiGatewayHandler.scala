163ab5d7ac8ca1dbeeb06afd6544245964278e57
Juan Manuel Torres <kinojman@gmail.com>
2018-03-28 21:41:14 -0700

    Various updates. Extends lambda configuration interface, simplified get request from input stream, and remove getting the account number. This is now an extension on the context.

--- src/main/scala/onema/serverlessbase/function/ApiGatewayHandler.scala
+++ src/main/scala/onema/serverlessbase/function/ApiGatewayHandler.scala
@@ -1,42 +1,48 @@
+/**
+  * This file is part of the ONEMA onema.serverlessbase Package.
+  * For the full copyright and license information,
+  * please view the LICENSE file that was distributed
+  * with this source code.
+  *
+  * copyright (c) 2018, Juan Manuel Torres (http://onema.io)
+  *
+  * @author Juan Manuel Torres <kinojman@gmail.com>
+  */
 package onema.serverlessbase.function
 
 
 import java.io.{InputStream, OutputStream}
 import java.nio.charset.Charset
-import java.util.Scanner
 
+import com.amazonaws.regions.Regions
 import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse
-import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder
+import com.amazonaws.services.sns.AmazonSNSAsync
 import com.typesafe.scalalogging.Logger
 import onema.core.json.Implicits._
+import onema.serverlessbase.configuration.lambda.LambdaConfiguration
 import onema.serverlessbase.exception.HandleRequestException
 import onema.serverlessbase.exception.ThrowableExtensions._
 import onema.serverlessbase.model.{ApiGatewayProxyRequest, ErrorMessage}
 import org.apache.http.HttpStatus
 
+import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 
-trait ApiGatewayHandler extends ApiGatewayResponse {
+trait ApiGatewayHandler extends ApiGatewayResponse with LambdaConfiguration {
 
   //--- Fields ---
   protected val log = Logger("apigateway-handler")
 
-  protected var lambdaContext: Context = _
+  protected val region: Regions = Regions.fromName(sys.env.getOrElse("AWS_REGION", "us-east-1"))
 
-  protected lazy val accountId: String = lambdaContext.getInvokedFunctionArn.split(':')(4)
+  private val snsErrorTopic = getValue("/sns/error/topic")
 
-  private val snsErrorTopic = sys.env.get("SNS_ERROR_TOPIC")
-
-  private val region = sys.env.get("AWS_REGION")
-
-  private val snsClient = AmazonSNSAsyncClientBuilder.defaultClient()
+  protected val snsClient: AmazonSNSAsync
 
   //--- Methods ---
   protected def getRequest(inputStream: InputStream): ApiGatewayProxyRequest = {
-    val scanner = new Scanner(inputStream, "utf-8")
-    val json: String = scanner.useDelimiter("\\A").next()
+    val json = Source.fromInputStream(inputStream).mkString
     log.info(json)
     json.jsonParse[ApiGatewayProxyRequest]
   }
@@ -61,7 +67,7 @@ trait ApiGatewayHandler extends ApiGatewayResponse {
         ex.httpResponse
 
       // General exception, handle it gracefully
-      case ex =>
+      case _ =>
 
         // report error to SNS Topic
         if(snsErrorTopic.isDefined) {
@@ -69,7 +75,7 @@ trait ApiGatewayHandler extends ApiGatewayResponse {
         }
 
         // generate response to send back to the api user
-        buildError(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal server errors, check the logs for more information.")
+        buildError(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error: check the logs for more information.")
     }
   }
 }
