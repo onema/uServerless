105d7cf1fc3616cd24265ed2fa613559141bb670
Juan Manuel Torres <kinojman@gmail.com>
2018-03-28 21:45:44 -0700

    Requires lambda configuration and gets SNS topic using the new path notation.

--- src/main/scala/onema/serverlessbase/function/LambdaHandler.scala
+++ src/main/scala/onema/serverlessbase/function/LambdaHandler.scala
@@ -1,33 +1,36 @@
 /**
-  * This file is part of the ONEMA onema Package.
+  * This file is part of the ONEMA onema.serverlessbase Package.
   * For the full copyright and license information,
   * please view the LICENSE file that was distributed
   * with this source code.
   *
-  * copyright (c) 2017, Juan Manuel Torres (http://onema.io)
+  * copyright (c) 2018, Juan Manuel Torres (http://onema.io)
   *
   * @author Juan Manuel Torres <kinojman@gmail.com>
   */
 
 package onema.serverlessbase.function
 
-import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.sns.AmazonSNSAsync
 import com.typesafe.scalalogging.Logger
+import onema.serverlessbase.configuration.lambda.LambdaConfiguration
 import onema.serverlessbase.exception.ThrowableExtensions._
 
 import scala.util.{Failure, Success, Try}
 
-trait LambdaHandler {
+trait LambdaHandler extends LambdaConfiguration {
+
+  //--- Fields ---
+  protected val log: Logger = Logger("lambda-handler")
+
+  protected val region: Regions = Regions.fromName(sys.env.getOrElse("AWS_REGION", "us-east-1"))
+
+  private val snsErrorTopic = getValue("/sns/error/topic")
+
+  protected val snsClient: AmazonSNSAsync
 
   //--- Methods ---
-  protected def log = Logger("sns-handler")
-
-  private val snsErrorTopic = sys.env.get("SNS_ERROR_TOPIC")
-
-  private val region = sys.env.get("AWS_REGION")
-
-  private val snsClient = AmazonSNSAsyncClientBuilder.defaultClient()
-
   protected def handle(function: () => Unit): Unit = {
     Try(function()) match {
       case Success(_) =>
