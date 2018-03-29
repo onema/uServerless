ec3a622f19093d6ecc7247daa797d6282f416a8b
Juan Manuel Torres <kinojman@gmail.com>
2018-03-28 21:38:47 -0700

    Update header comment

--- src/main/scala/onema/serverlessbase/exception/HandleRequestException.scala
+++ src/main/scala/onema/serverlessbase/exception/HandleRequestException.scala
@@ -1,10 +1,10 @@
 /**
-  * This file is part of the ONEMA onema Package.
+  * This file is part of the ONEMA onema.serverlessbase Package.
   * For the full copyright and license information,
   * please view the LICENSE file that was distributed
   * with this source code.
   *
-  * copyright (c) 2017, Juan Manuel Torres (http://onema.io)
+  * copyright (c) 2018, Juan Manuel Torres (http://onema.io)
   *
   * @author Juan Manuel Torres <kinojman@gmail.com>
   */
