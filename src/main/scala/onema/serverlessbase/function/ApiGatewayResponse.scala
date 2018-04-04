35d7bd5d650009be15a8135f3a43a4cc66e5317a
Juan Manuel Torres <kinojman@gmail.com>
2018-04-03 23:12:16 -0700

    Adding more ways to build response with headers

--- src/main/scala/onema/serverlessbase/function/ApiGatewayResponse.scala
+++ src/main/scala/onema/serverlessbase/function/ApiGatewayResponse.scala
@@ -11,14 +11,25 @@
 
 package onema.serverlessbase.function
 
-import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse
 import onema.core.json.Implicits._
 import onema.serverlessbase.model.ErrorMessage
+import scala.collection.JavaConverters._
 
 
 trait ApiGatewayResponse {
 
   //--- Methods ---
+  protected def buildResponse(code: Int, headers: Map[String, String] = Map()): AwsProxyResponse = {
+    addHeaders(new AwsProxyResponse(code), headers)
+  }
+
+  protected def buildResponse(code: Int, payload: AnyRef, headers: Map[String, String]): AwsProxyResponse = {
+    val response = buildResponse(code, payload)
+    response.setHeaders(headers.asJava)
+    response
+  }
+
   protected def buildResponse(code: Int, payload: AnyRef): AwsProxyResponse = {
     val response = new AwsProxyResponse(code)
     response.setBody(payload.toJson)
@@ -27,5 +38,12 @@ trait ApiGatewayResponse {
 
   protected def buildError(code: Int, message: String): AwsProxyResponse = {
     buildResponse(code, ErrorMessage(message))
+  }
+  private def addHeaders(response: AwsProxyResponse, headers: Map[String, String]): AwsProxyResponse = {
+    if(headers.nonEmpty) {
+      new AwsProxyResponse(response.getStatusCode, headers.asJava)
+    } else {
+      response
+    }
   }
 }
