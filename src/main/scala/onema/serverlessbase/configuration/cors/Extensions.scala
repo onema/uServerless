7a7b4adaa9c6d6670fa8cfb3ade3d8301ea938c4
Juan Manuel Torres <kinojman@gmail.com>
2018-04-04 08:16:34 -0700

    Throwing a logic error if the origin is valid but it's value is not set.

--- src/main/scala/onema/serverlessbase/configuration/cors/Extensions.scala
+++ src/main/scala/onema/serverlessbase/configuration/cors/Extensions.scala
@@ -19,12 +19,16 @@ object Extensions {
   implicit class AwsProxyResponseExtension(response: AwsProxyResponse) {
     def withCors(corsConfiguration: CorsConfiguration): AwsProxyResponse = {
       if(corsConfiguration.isOriginValid) {
-        val existingHeaders = Option(response.getHeaders.asScala).getOrElse(Map())
-        val headers = Map(
-          "Access-Control-Allow-Origin" -> corsConfiguration.origin.get,
-          "Access-Control-Allow-Credentials" -> "true"
-        ) ++ existingHeaders
-        response.setHeaders(headers.asJava)
+        corsConfiguration.origin match {
+          case Some(origin) =>
+            val existingHeaders = Option(response.getHeaders.asScala).getOrElse(Map())
+            val headers = Map(
+              "Access-Control-Allow-Origin" -> corsConfiguration.origin.get,
+              "Access-Control-Allow-Credentials" -> "true"
+            ) ++ existingHeaders
+            response.setHeaders(headers.asJava)
+          case None => throw new RuntimeException("Logic error: The origin was mark as valid, yet it's value is empty")
+        }
       }
       response
     }
