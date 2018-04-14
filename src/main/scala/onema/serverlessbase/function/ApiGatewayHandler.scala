d0222b09ffc6893c4f5762fd734e1075ed8743b5
Juan Manuel Torres <kinojman@gmail.com>
2018-04-14 14:49:42 -0700

    The LambdaHandler now uses type parameters. ApiGatewayHandler extends from the lambda handler to do error reporting.

--- src/main/scala/onema/serverlessbase/function/ApiGatewayHandler.scala
+++ src/main/scala/onema/serverlessbase/function/ApiGatewayHandler.scala
@@ -8,13 +8,13 @@
   *
   * @author Juan Manuel Torres <kinojman@gmail.com>
   */
+
 package onema.serverlessbase.function
 
 
 import java.io.{InputStream, OutputStream}
 import java.nio.charset.Charset
 
-import com.amazonaws.regions.Regions
 import com.amazonaws.serverless.proxy.model.{AwsProxyRequest, AwsProxyResponse}
 import com.amazonaws.services.sns.AmazonSNSAsync
 import com.typesafe.scalalogging.Logger
@@ -22,21 +22,18 @@ import onema.core.json.Implicits._
 import onema.serverlessbase.configuration.lambda.LambdaConfiguration
 import onema.serverlessbase.exception.HandleRequestException
 import onema.serverlessbase.exception.ThrowableExtensions._
-import onema.serverlessbase.model.ErrorMessage
 import org.apache.http.HttpStatus
 
 import scala.io.Source
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 
-trait ApiGatewayHandler extends ApiGatewayResponse with LambdaConfiguration {
+trait ApiGatewayHandler extends LambdaHandler[AwsProxyResponse]
+  with ApiGatewayResponse
+  with LambdaConfiguration {
 
   //--- Fields ---
-  protected val log = Logger("apigateway-handler")
-
-  protected val region: Regions = Regions.fromName(sys.env.getOrElse("AWS_REGION", "us-east-1"))
-
-  private val snsErrorTopic = getValue("/sns/error/topic")
+  override protected val log = Logger("apigateway-handler")
 
   protected val snsClient: AmazonSNSAsync
 
@@ -53,29 +50,20 @@ trait ApiGatewayHandler extends ApiGatewayResponse with LambdaConfiguration {
     outputStream.close()
   }
 
-  protected def handle(function: => AwsProxyResponse): AwsProxyResponse = {
-    Try(function) match {
-      case Success(response) => response
-      case Failure(e: Throwable) => handleFailure(e)
-    }
-  }
-
-  protected def handleFailure(exception: Throwable): AwsProxyResponse = {
+  override protected def handleFailure(exception: Throwable): AwsProxyResponse = {
     val message = s"Internal Server Error: ${exception.message}"
     log.error(message)
     exception match {
       case ex: HandleRequestException =>
-        ex.httpResponse
+        buildError(ex.code, ex.getMessage)
 
       // General exception, handle it gracefully
-      case _ =>
+      case ex =>
 
         // report error to SNS Topic
-        if(snsErrorTopic.isDefined) {
-          snsClient.publish(snsErrorTopic.get, ErrorMessage(message).toJson)
-        }
+        Try(super.handleFailure(ex))
 
-        // generate response to send back to the api user
+        // Generate response to send back to the api user
         buildError(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error: check the logs for more information.")
     }
   }
