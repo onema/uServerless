7914f9481df604727e32df2eed30fc6c1bcb3a42
Juan Manuel Torres <kinojman@gmail.com>
2018-09-09 17:08:13 -0700

    Deleteing script and downloading it from public bucket instead!

--- submit_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "${CODEBUILD_SOURCE_VERSION}" =~ [0-9a-f]{5,40} ]] ; then
-  echo "Generating coverage report"
-  sbt coverageReport;
-  echo "Publishing coverage report"
-  sbt codacyCoverage;
-fi
